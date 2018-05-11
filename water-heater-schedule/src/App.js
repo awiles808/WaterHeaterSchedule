ee2f5d4bfa1c065a372b6abfc47d542bcc82f1b7
awiles808 <awiles808@gmail.com>
2018-05-11 10:42:35 -0600

    Cleaned up routes path

--- water-heater-schedule/src/App.js
+++ water-heater-schedule/src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import CustomerInfo from './components/CustomerInfo';
 import Footer from './components/Footer';
@@ -10,11 +9,9 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-      {/* <HomepageContainer /> */}
-      {/* <WaterHeaterInfo /> */}
+
       { MyRoutes }
-      {/* <CustomerInfo />
-      <Footer /> */}
+
       </div>
     );
   }
